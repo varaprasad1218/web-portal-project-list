c800b6bc79ae0a328695927a59d8dce98f64120b
varaprasad <56168754+varaprasad1218@users.noreply.github.com>
2020-12-31 10:57:26 +0530

    Update add1.php

--- add1.php
+++ add1.php
@@ -1,7 +1,7 @@
 <html>
 <head>
          <br></br>
-	<title>Add project details</title>
+	<title>Add details</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
 </head>
 
